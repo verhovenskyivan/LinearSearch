b6f365db888bb472a634f63b6a21fbce8af09729
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-10 12:16:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+static void Main(string[] args)
+{
+    static int FindIndex(int[] array, int index)
+    {
+        for (int i = 0; i < array.Length; i++)      
+            if (array[i] == index)
+               return i;
+        return -1;            
+    }
+}
